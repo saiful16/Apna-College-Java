9b29c16604658e4751373834ead67bd1a369152e
saiful16 <110138463+saiful16@users.noreply.github.com>
2023-05-24 21:45:14 +0200

    while loops

--- Loops.java
+++ Loops.java
@@ -1,2 +1,7 @@
-package PACKAGE_NAME;public class Loops {
+public class Loops {
+    public static void main(String[] args) {
+        for (int i=0; i < 10; i++){
+            System.out.println("Hello World");
+        }
+    }
 }
