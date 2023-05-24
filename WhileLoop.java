c3464eea2af3a73f6e3e5230e0410c4bb343ce50
saiful16 <110138463+saiful16@users.noreply.github.com>
2023-05-24 21:50:01 +0200

    do while

--- WhileLoop.java
+++ WhileLoop.java
@@ -1,9 +1,15 @@
 public class WhileLoop {
     public static void main(String[] args) {
-        int i = 0;
-        while (i<11){
+//        int i = 0;
+//        while (i<11){
+//            System.out.println("hello world");
+//            i++;
+//        }
+        int i =0;
+        do{
             System.out.println("hello world");
             i++;
         }
+        while (i<10);
     }
 }
