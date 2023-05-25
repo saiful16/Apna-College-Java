ebf36839c80e8bd9e7ce03de30d0558f9a544b61
saiful16 <110138463+saiful16@users.noreply.github.com>
2023-05-25 19:24:25 +0200

    Print a solid rectengle

---
+++ SolidRectengle.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class SolidRectengle {
+    public static void main(String[] args) {
+        System.out.println("Enter the number of Columnn : ");
+        Scanner sc = new Scanner(System.in);
+        int column = sc.nextInt();
+        System.out.println("Enter the number of Row :");
+        int row = sc.nextInt();
+
+        for (int i = 1; i <= row; i++){
+            for (int j= 1; j<= column;  j++){
+
+            }
+        }
+    }
+}
