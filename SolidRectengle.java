5eaa7525dc46f658df4348ad2269e206050adf39
saiful16 <110138463+saiful16@users.noreply.github.com>
2023-05-25 19:35:29 +0200

    solution of printing a solid rectengle

--- SolidRectengle.java
+++ SolidRectengle.java
@@ -10,8 +10,9 @@ public class SolidRectengle {
 
         for (int i = 1; i <= row; i++){
             for (int j= 1; j<= column;  j++){
-
+                System.out.print("*");
             }
+            System.out.println();
         }
     }
 }
