9094f0568ce216bfe502235a463601cdf16b69bc
saiful16 <110138463+saiful16@users.noreply.github.com>
2023-05-26 13:39:50 +0200

    solution of half pyramid

--- HalfPiramid.java
+++ HalfPiramid.java
@@ -8,7 +8,10 @@ public class HalfPiramid {
 
         for (int i =1; i <=row; i++){
             for (int j =1; j<i; j++){
-
+                System.out.print("*");
+            }
+            System.out.println();
+        }
 
     }
 }
