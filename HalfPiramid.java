0eb292be841f3adbe91336ed127d2415687e6e10
saiful16 <110138463+saiful16@users.noreply.github.com>
2023-05-26 13:38:41 +0200

    half pyramid problem

---
+++ HalfPiramid.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class HalfPiramid {
+    public static void main(String[] args) {
+        System.out.println("Enter the number of row : ");
+        Scanner sc = new Scanner(System.in);
+        int row = sc.nextInt();
+
+        for (int i =1; i <=row; i++){
+            for (int j =1; j<i; j++){
+
+
+    }
+}
