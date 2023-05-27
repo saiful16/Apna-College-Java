3311b4bf262b96758ccc6b84b534f9d7c9cebfbf
saiful16 <110138463+saiful16@users.noreply.github.com>
2023-05-28 00:37:43 +0200

    solution of inverted half pyramid

--- InvertedHalfPyramid.java
+++ InvertedHalfPyramid.java
@@ -3,10 +3,10 @@ import java.util.Scanner;
 public class InvertedHalfPyramid {
     public static void main(String[] args) {
         System.out.println("Enter the number of row : ");
-        Scanner sc = new Scanner(System.in);
-        int row = sc.nextInt();
 
-
+        for (int i =5; i >=1; i--){
+            for (int j =1; j<=i; j++){
+                System.out.print("*");
             }
             System.out.println();
         }
