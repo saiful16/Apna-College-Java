60c5d15f64995fdeccea25bb12f8ca73878af132
saiful16 <110138463+saiful16@users.noreply.github.com>
2023-05-27 23:36:05 +0200

    Inverted half pyramid

---
+++ InvertedHalfPyramid.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class InvertedHalfPyramid {
+    public static void main(String[] args) {
+        System.out.println("Enter the number of row : ");
+        Scanner sc = new Scanner(System.in);
+        int row = sc.nextInt();
+
+
+            }
+            System.out.println();
+        }
+
+    }
+}
