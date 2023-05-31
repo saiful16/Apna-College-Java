cce63367a78e5bf0e5944c3d06007e6192a36224
saiful16 <110138463+saiful16@users.noreply.github.com>
2023-05-31 23:51:18 +0200

    solution

--- InvertedHalfPyramidWithNumbers.java
+++ InvertedHalfPyramidWithNumbers.java
@@ -2,7 +2,7 @@ public class InvertedHalfPyramidWithNumbers {
     public static void main(String[] args) {
         int n = 5;
          for (int i= 1; i <=n; i++){
-             for (int j =1; j <=n-i; j++){
+             for (int j =1; j <=n-i+1; j++){
                  System.out.print(j);
              }
              System.out.println();
