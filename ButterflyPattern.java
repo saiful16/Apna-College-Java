fe9958a24c5a89d09333a821653d1874502126d3
saiful16 <110138463+saiful16@users.noreply.github.com>
2023-06-01 21:36:11 +0200

    half solution is done

--- ButterflyPattern.java
+++ ButterflyPattern.java
@@ -2,12 +2,20 @@ public class ButterflyPattern {
     public static void main(String[] args) {
         int n =4;
 
-        for (int i =1; i <=n; i++){
-            for ( int j =1; j<=i; j++){
+        for (int i =1; i <=n;  i++){
+            for (int j =1; j<=i; j++){
                 System.out.print("*");
-
+            }
+            int spaces = 2 * (n-i);
+            for (int j = 1; j<= spaces; j++){
+                System.out.print(" ");
+            }
+            for (int j =1; j<=i; j++){
+                System.out.print("*");
             }
             System.out.println();
         }
+
+
     }
 }
