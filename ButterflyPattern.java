535fb9faa077b233fb402453cfe18b08c44ddd50
saiful16 <110138463+saiful16@users.noreply.github.com>
2023-06-01 22:02:11 +0200

    solution of butterfly pattern

--- ButterflyPattern.java
+++ ButterflyPattern.java
@@ -16,6 +16,20 @@ public class ButterflyPattern {
             System.out.println();
         }
 
+        for (int i =n; i >=1;  i--){
+            for (int j =1; j<=i; j++){
+                System.out.print("*");
+            }
+            int spaces = 2 * (n-i);
+            for (int j = 1; j<= spaces; j++){
+                System.out.print(" ");
+            }
+            for (int j =1; j<=i; j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+
 
     }
 }
