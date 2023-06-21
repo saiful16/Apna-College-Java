516dac2716c425d88628320e015212da380601f2
saiful16 <110138463+saiful16@users.noreply.github.com>
2023-06-21 23:50:24 +0200

    first function

--- FunctionAndMethod.java
+++ FunctionAndMethod.java
@@ -1,2 +1,15 @@
-package PACKAGE_NAME;public class FunctionAndMethod {
+import java.util.Scanner;
+
+public class FunctionAndMethod {
+    public static void printmyname( String name){
+        System.out.println(name);
+        return;
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String name = sc.nextLine();
+
+        printmyname(name);
+    }
 }
+
