950eab8d094a68f839c55ece639b64031b099acf
saiful16 <110138463+saiful16@users.noreply.github.com>
2023-06-22 13:50:15 +0200

    take user input and give pu

---
+++ UserInputInArrays.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class UserInputInArrays {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        int size = sc.nextInt();
+        int nunber[]= new int[size];
+
+        for (int i=0; i <size; i++){
+            nunber[i]= sc.nextInt();
+
+        }
+
+        for (int i = 0; i <size; i++){
+            System.out.println(nunber[i]);
+        }
+    }
+
+}
