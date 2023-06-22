861a48276bc462c0833d0459c122af077738fae3
saiful16 <110138463+saiful16@users.noreply.github.com>
2023-06-22 13:09:45 +0200

    first array

---
+++ FirstArray.java
@@ -0,0 +1,17 @@
+public class FirstArray {
+    public static void main(String[] args) {
+
+        int marks[] = new int[3];
+        marks[0]= 99;
+        marks[1]= 93;
+        marks[2]= 90;
+
+//        System.out.println(marks[0]);
+//        System.out.println(marks[1]);
+//        System.out.println(marks[2]);
+
+        for (int i = 0; i <3; i++){
+            System.out.println(marks[i]);
+        }
+    }
+}
